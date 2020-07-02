73e096ded1ff8edf3b06d6c168449e8600cda989
Makarov-Ivan <makivanik@gmail.com>
2020-07-03 02:09:11 +0300

    togle grid and list buttons

--- src/app/pokemon/pokemon-header/pokemon-header.component.ts
+++ src/app/pokemon/pokemon-header/pokemon-header.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pokemon-header',
   templateUrl: './pokemon-header.component.html',
-  styleUrls: ['./pokemon-header.component.scss']
+  styleUrls: ['./pokemon-header.component.scss'],
 })
 export class PokemonHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  isListed = false;
+  ngOnInit(): void {}
+  clickHendle(): void {
+    this.isListed = !this.isListed;
+    console.log(`listed: ${this.isListed}`);
   }
-
 }
