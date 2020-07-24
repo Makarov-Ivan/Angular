c1c4e8b286973f49e110d9c8df859cbe4958a284
Makarov-Ivan <makivanik@gmail.com>
2020-07-24 23:22:00 +0300

    pokemon-card-item small refactoring

--- src/app/pokemon/pokemon-card-item/pokemon-card-item.component.ts
+++ src/app/pokemon/pokemon-card-item/pokemon-card-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import { Pokemons } from '../pokemon.list';
-import { Pokemon } from "../pokemon.interface";
-import { PokemonListService } from "../pokemon-list.service";
+import { Pokemon } from '../pokemon.interface';
+import { PokemonListService } from '../pokemon-list.service';
 import { IsListService } from '../is-list.service';
 
 @Component({
@@ -9,12 +9,17 @@ import { IsListService } from '../is-list.service';
   templateUrl: './pokemon-card-item.component.html',
   styleUrls: ['./pokemon-card-item.component.scss'],
 })
+
 export class PokemonCardItemComponent implements OnInit {
+
   constructor(public isListService: IsListService, private pokemonService: PokemonListService) { }
+
   pokemons: Pokemon[];
+
   getPokemons(): void {
     this.pokemons = this.pokemonService.getAll();
   }
+
   ngOnInit(): void {
     this.getPokemons();
   }
