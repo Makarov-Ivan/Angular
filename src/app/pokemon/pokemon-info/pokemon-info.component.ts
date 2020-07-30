e45d1f9253d3a6fb8177454ea7d202192bc92d4b
Makarov-Ivan <makivanik@gmail.com>
2020-07-30 20:35:10 +0300

    added logic in ts file

--- src/app/pokemon/pokemon-info/pokemon-info.component.ts
+++ src/app/pokemon/pokemon-info/pokemon-info.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { PokemonListService } from '../pokemon-list.service';
+import { Pokemon } from '../pokemon.interface';
 
 @Component({
   selector: 'app-pokemon-info',
   templateUrl: './pokemon-info.component.html',
   styleUrls: ['./pokemon-info.component.scss']
 })
-export class PokemonInfoComponent implements OnInit {
+export class PokemonInfoComponent {
 
-  constructor() { }
+  constructor(
 
-  ngOnInit(): void {
+    private _Activatedroute: ActivatedRoute,
+    private _router: Router,
+    private _PokemonListService: PokemonListService
+  ) { }
+
+  sub;
+  id;
+  pokemon: Pokemon;
+  btnValue = 'Catch!';
+  isCathed = false;
+
+  clcikHandler(name: string): void {
+    if (this.isCathed) {
+      this.btnValue = 'Catch!';
+      this.isCathed = !this.isCathed;
+      console.log(`${name} was released`);
+    } else {
+      this.btnValue = 'Release!';
+      this.isCathed = !this.isCathed;
+      console.log(`${name} was cathced`);
+    }
   }
 
+  ngOnInit(): void {
+    this.sub = this._Activatedroute.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      this.pokemon = this._PokemonListService.getById(this.id);
+    });
+    this.pokemon.date = this.pokemon.date ? this.pokemon.date : new Date().toLocaleDateString();
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+  onBack(): void {
+    this._router.navigate(['']);
+  }
 }
